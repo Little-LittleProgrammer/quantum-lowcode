86b5023a7c40cca9fbfc8738e3b7b8a1887bb2e6
Evan Wu <850123119@qq.com>
2023-12-19 10:55:27 +0800

    feat: 全局数据管理

---
+++ packages/data-source/index.ts
@@ -0,0 +1,6 @@
+export {DataSourceManager} from './src/data-source-manager';
+
+export {DataSource} from './src/data-source/base';
+export {HttpDataSource} from './src/data-source/http';
+
+export type * from './src/types';
--- packages/data-source/src/data-source-manager.ts
+++ packages/data-source/src/data-source-manager.ts
@@ -1,5 +1,5 @@
 import { IDataSourceSchema, ILowCodeRoot } from '@qimao/quantum-schemas';
-import { Subscribe } from '@qimao/quantum-utils';
+import { Subscribe, js_is_function } from '@qimao/quantum-utils';
 import { IDataSourceManagerData, IDataSourceManagerOptions, IHttpDataSourceSchema } from './types';
 import { DataSource } from './data-source/base';
 import { HttpDataSource } from './data-source/http';
@@ -24,6 +24,7 @@ export class DataSourceManager extends Subscribe {
         if (!config) return;
 
         let ds: DataSource;
+        // 如果是 http 类型
         if (config.type === 'http') {
             ds = new HttpDataSource({
                 app: this.app,
@@ -32,6 +33,7 @@ export class DataSourceManager extends Subscribe {
                 request: this.app.request,
             });
         } else {
+            // 如果是基本类型
             const DataSourceClass = DataSource;
 
             ds = new DataSourceClass({
@@ -46,11 +48,57 @@ export class DataSourceManager extends Subscribe {
         this.data[ds.id] = ds.data;
 
         ds.emit('change', () => this.setData(ds));
+
+        // 处理初始化数据逻辑
+        const beforeInit: ((...args: any[]) => any)[] = [];
+        const afterInit: ((...args: any[]) => any)[] = [];
+
+        ds.methods.forEach((method) => {
+            if (!js_is_function(method.content)) return;
+            switch (method.timing) {
+                case 'beforeInit':
+                    beforeInit.push(method.content as (...args: any[]) => any);
+                    break;
+                case 'afterInit':
+                    afterInit.push(method.content as (...args: any[]) => any);
+            }
+        });
+
+        for (const method of beforeInit) {
+            await method({ params: {}, dataSource: ds, app: this.app, });
+        }
+
+        await ds.init();
+
+        for (const method of afterInit) {
+            await method({ params: {}, dataSource: ds, app: this.app, });
+        }
     }
 
     public setData(ds: DataSource) {
         Object.assign(this.data[ds.id], ds.data);
         this.emit('change', ds.id);
+    }
+
+    public updateSchema(schemas: IDataSourceSchema[]) {
+        schemas.forEach((schema) => {
+            const ds = this.dataSourceMap.get(schema.id);
+            if (!ds) {
+                return;
+            }
+
+            this.removeDataSource(schema.id);
+
+            this.addDataSource(schema);
+        });
+    }
+
+    public removeDataSource(id: string) {
+        const ds = this.dataSourceMap.get(id);
+        if (!ds) return;
+        ds.destroy();
+        this.dataSourceMap.delete(id);
+        delete this.data[id];
     }
 
     public destroy() {
