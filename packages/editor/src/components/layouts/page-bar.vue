2daaca9e78e4066cf316af288b7bcb2685ed110f
EvanWu <850123119@qq.com>
2024-02-27 09:43:10 +0800

    feat: 完成page-bar功能

--- packages/editor/src/components/layouts/page-bar.vue
+++ packages/editor/src/components/layouts/page-bar.vue
@@ -1,18 +1,32 @@
 <!--  -->
 <template>
-    <a-tabs size="small" type="card" :active-key="(page && page.field) as string" @change="switch_page">
-        <tab-pane
-            v-for="item in (root && root.children) || []"
-            :key="item.field"
-            :tab="item.field"
-        ></tab-pane>
-    </a-tabs>
+    <div>
+        <a-tabs size="small" type="card" :active-key="(page && page.field) as string" @change="switchPage">
+            <tab-pane
+                v-for="item in (root && root.children) || []"
+                :key="item.field"
+            >
+                <template #tab>
+                    <q-antd-dropdown :dropMenuList="dropDownList" @menuEvent="(menu) => handleMenuEvent(menu, item)">
+                        {{ item.field }}
+                    </q-antd-dropdown>
+                </template>
+            </tab-pane>
+            <template #rightExtra>
+                <a-button @click="addPage">+</a-button>
+            </template>
+        </a-tabs>
+
+    </div>
 </template>
 
 <script lang='ts' setup>
 import { IServices } from '../../types';
-import { computed, inject } from 'vue';
+import { computed, inject, ref, toRaw } from 'vue';
 import {Tabs as ATabs, TabPane} from 'ant-design-vue';
+import { DropMenu } from '@q-front-npm/vue3-antd-pc-ui';
+import { ISchemasPage, NodeType } from '@qimao/quantum-schemas';
+import props from 'ant-design-vue/es/dropdown/props';
 defineOptions({
     name: 'QEditorPageBar',
 });
@@ -24,8 +38,42 @@ const root = computed(() => editorService?.get('root'));
 
 const page = computed(() => editorService?.get('page'));
 
-function switch_page(key: string) {
+const dropDownList = ref([
+    {
+        icon: 'CopyOutlined',
+        event: 'copy',
+        text: '复制',
+    },
+    {
+        icon: 'CloseOutlined',
+        event: 'delete',
+        text: '删除',
+        disabled: (root.value?.children?.length || 0) <= 1,
+    }
+]);
+
+function handleMenuEvent(menu: DropMenu, item: ISchemasPage): void {
+    const { event, } = menu;
+    if (event === 'copy') {
+        item && editorService?.copy(item);
+    } else {
+        editorService?.delete(item);
+    }
+}
+
+function switchPage(key: string) {
     editorService?.select(key);
+}
+
+function addPage() {
+    if (!editorService) return;
+    const root = toRaw(editorService.get('root'));
+    if (!root) throw new Error('root 不能为空');
+    const pageConfig = {
+        type: NodeType.PAGE,
+        children: [],
+    };
+    editorService.add(pageConfig);
 }
 
 </script>
