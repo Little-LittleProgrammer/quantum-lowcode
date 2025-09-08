60071499098711c4d3ddf34e82651f72c82b5583
EvanWu <850123119@qq.com>
2025-09-08 17:01:27 +0800

    feat: 移除旧的 commitlint 配置文件，新增 commitlint.config.mjs 文件以支持新的配置方式，同时更新相关依赖至 3.0.0-beta.0 版本，优化项目的代码结构和可维护性。

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,1 @@
-module.exports = {
-    printWidth: 200, // 此长度跟eslint  'max-len': [2, 500], 保持一致
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'always',
-};
+export { default } from '@quantum-design-configs/prettier';
